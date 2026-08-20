6d455e1dc6f1ba981e26f22b9b2f2b2921d58752
HamdaneBrini <hamdenbrini01@gmail.com>
2026-08-20 20:58:41 +0100

    feat: Enhance MPU configuration with memory access control and region management

--- Kernel/Inc/mpu.h
+++ Kernel/Inc/mpu.h
@@ -1,9 +1,32 @@
 #ifndef MPU_H
 #define MPU_H
 
+#include <stddef.h>
+#include <stdint.h>
 #include "config.h"
+#include "stm32h5xx_hal.h"
+
+typedef enum {
+    MEMORY_NO_ACCESS,
+    MEMORY_READ_ONLY,
+    MEMORY_READ_WRITE,
+    MEMORY_READ_ONLY_PV,
+    MEMORY_READ_WRITE_PV,
+} MemoryAccess_t;
+
+typedef enum {
+    MEMORY_REGION_NUMEBER_0,
+    MEMORY_REGION_NUMEBER_1,
+    MEMORY_REGION_NUMEBER_2,
+    MEMORY_REGION_NUMEBER_3,
+    MEMORY_REGION_NUMEBER_4,
+    MEMORY_REGION_NUMEBER_5,
+    MEMORY_REGION_NUMEBER_6,
+} MemoryRegionNumber_t;
 
 /* Protect the linker-defined kernel RAM region from unprivileged access. */
-TinyStatus_t tiny_mpu_config(void);
+TinyStatus_t MPU_kernel_config(void);
+TinyStatus_t MPU_config(uint32_t base_address, uint32_t limit_address, MemoryAccess_t access, int is_executable,
+                        MemoryRegionNumber_t region_number);
 
 #endif /* MPU_H */
